8120b7607256e5f4c2846b866d8519501ed4e1ca
caauribegu <caauribegu@unal.edu.co>
2022-06-22 22:07:00 -0500

    Datalake & Ingest

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -4,7 +4,7 @@ def create_data_lake():
     Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
     las siguientes subcarpetas:
 
-    ```
+
     .
     |
     \___ data_lake/
@@ -17,14 +17,34 @@ def create_data_lake():
               |___ features/
               |___ forecasts/
 
-    ```
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+
+
+# El motivo de la función es la creación del "datalake" que se tiene dispuesto a utilizar en la realización del producto de datos. 
+# Se crean las carpetas mostradas anteriormente y se ejecuta en la rama donde se quiera crear la carpeta. 
+
+
+    from os import mkdir
+
+    mkdir("data_lake")
+    mkdir("data_lake/landing")
+    mkdir("data_lake/raw")
+    mkdir("data_lake/cleansed")
+    mkdir("data_lake/business")
+    mkdir("data_lake/business/reports")
+    mkdir("data_lake/business/reports/figures")
+    mkdir("data_lake/business/features")
+    mkdir("data_lake/business/forescasts")
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+
+    create_data_lake()
